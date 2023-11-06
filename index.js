779a20ec69e832a10c536479ea9e00bd1ee4dff9
Roshan Kumar <rsahu7989@gmail.com>
2023-11-06 22:50:37 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
+const { Server } = require('socket.io');
+const PORT = process.env.PORT || 8000;
+
+const io = new Server(PORT, {
+  cors: true
+});
+
+const EmailtoSocketIdMap = new Map();
+const SocketIdtoEmailMap = new Map();
+
+io.on('connection', (socket) => {
+  
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    EmailtoSocketIdMap.set(email, socket.id);
+    SocketIdtoEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", {
+      email,
+      id: socket.id
+    });
+    socket.join(room);
+    io.to(socket.id).emit('room:join', data);
+  });
+
+  socket.on('user:call', ({ to, offer }) => {
+    io.to(to).emit('incomming:call', { from: socket.id, offer });
+  });
+
+  socket.on('call:accepted', ({ to, ans }) => {
+    io.to(to).emit('call:accepted', { from: socket.id, ans });
+  });
+
+  socket.on('peer:nego:needed', ({ to, offer }) => {
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on('peer:nego:done', ({ to, ans }) => {
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
